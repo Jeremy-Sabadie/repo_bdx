5941f2e3757420dc713e47c41f1b4eaffe8c95aa
Simon Wyckaert <simon@digitold.com>
2018-01-08 14:23:22 +0100

    correction erreur syntaxe puts

--- simon.rb
+++ simon.rb
@@ -1,3 +1,3 @@
-put "Bonjour je m'appelle Simon. Tu veux être mon ami ? Comment tu t'appelles ?"
+puts "Bonjour je m'appelle Simon. Tu veux être mon ami ? Comment tu t'appelles ?"
 moncopain = gets.chomp
-put "Bonjour #{moncopain} !"
+puts "Bonjour #{moncopain} !"
